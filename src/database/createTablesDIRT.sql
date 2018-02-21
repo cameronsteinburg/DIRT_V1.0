46c1b291890df636497388a6dc71b25c54b42ada
MattCMacD <MattCMacD@MattCMacD-PC.hitronhub.home>
2018-02-20 17:07:53 -0700

    Matthew: database/createTablesDIRT.sql
    Added a description of what the file does.
    File can now be run to update the database architecture even if the tables and db already exist.

--- src/database/createTablesDIRT.sql
+++ src/database/createTablesDIRT.sql
@@ -1,7 +1,16 @@
+#This is the script that needs to be run on the MySQL server to create the database and all of the tables for it.
+#Once complete it will only need to be run once on a fresh MySQL server install.
+#This script also updates the database whenever new tables are added to it.
+
+
 #creates new user and grants them privs to do anything they want
 
 #GRANT ALL PRIVILEGES ON *.* TO 'USERNAME'@'localhost' IDENTIFIED BY 'PASS';
 
+
+#Remove for update
+drop table clients;
+drop database DIRT;
 
 #creates db
 create database DIRT;
