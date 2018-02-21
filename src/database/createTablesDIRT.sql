e82e5e261418b57a0489289bb04aec949cb80ae4
MattCMacD <MattCMacD@MattCMacD-PC.hitronhub.home>
2018-02-21 16:19:58 -0700

    Matthew: database/createTablesDIRT.sql
    Added table creation for the projects table, contains project data, pk, fk, few not nulls, and no other constraints.

--- src/database/createTablesDIRT.sql
+++ src/database/createTablesDIRT.sql
@@ -9,6 +9,7 @@
 
 
 #Remove for update
+drop table projects;
 drop table clients;
 drop database DIRT;
 
@@ -21,3 +22,6 @@ use DIRT;
 
 #creates client table
 create table Clients (clientNum smallint unsigned not null auto_increment, name varchar(50) not null, description varchar(5000), phone1 varchar(11) not null, phone2 varchar(11), email varchar(30), address varchar (50), isActive boolean not null, constraint pk_Client primary key (clientNum));
+
+#creates projects table
+create table Projects (projectNum smallint unsigned not null auto_increment, clientNum smallint unsigned not null, projectName varchar(50), projectType varchar(1), description varchar(5000), startDate Date, estimatedEndDate Date, clientOwing numeric(8,2), clientPaid numeric(8,2), estimatedShoppingCost numeric(8,2), estimatedLabourCost numeric(8,2), estimatedDeliveryCost numeric(8,2), allowanceCost numeric(8,2), actualShoppingCost numeric(8,2), actualLabourCost numeric(8,2), actualDeliveryCost numeric(8,2), extraneousExpenses numeric(8,2), estimatedProfit numeric(8,2), actualProfit numeric(8,2), actualEndDate Date, isActive boolean not null, constraint pk_Project primary key (projectNum), constraint fk_ProjectClient foreign key (clientNum) references Clients (clientNum));
