a9d7f0aa2cb56e5c092f4b8c51563b60efaa0fb3
734972 <734972@ELF6IT215940.ACDM.DS.SAIT.CA>
2018-02-20 21:50:20 -0700

    Cam: changed phone1 and 2 from 10 digits to 11 in db. extra data validation for new client, this is code that will carry over to similar scenarios

--- src/controller/CreateClientGUIController.java
+++ src/controller/CreateClientGUIController.java
@@ -18,10 +18,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 
-/**
- *
- * @author 645011, 734972
- */
 public class CreateClientGUIController implements Initializable {
 
     @FXML
@@ -44,6 +40,8 @@ public class CreateClientGUIController implements Initializable {
     private AnchorPane rootPane;
     @FXML
     private Label errorMessage;
+    @FXML
+    private Label invalidEmailMessage;
 
     @FXML
     private void cancelBtnAction(ActionEvent event) {
@@ -52,26 +50,45 @@ public class CreateClientGUIController implements Initializable {
 
     @FXML
     private void saveBtnAction(ActionEvent event) throws IOException {
-     
+
+        errorMessage.setVisible(false); //reset messages for multiple attempts bu user to get their data correct
+
         String name = nameField.getText();
         String email = emailField.getText();
         String address = addressField.getText();
         String description = descField.getText();
         
-        if(name.isEmpty() || phone1Field.getText().isEmpty()){
+        
+        //data validation commences 
+        if (name.isEmpty() || phone1Field.getText().isEmpty()) { //checking to see if the user entered blank data for not null fields
+            
+            errorMessage.setVisible(true);
+            return;
+        }
+
+        if (email.isEmpty() == false && (email.contains("@") == false || email.contains(".") == false)){ //checking that user entered valid email address format
+            
+            errorMessage.setText("Not a valid Email address");
             errorMessage.setVisible(true);
             return;
         }
         
+        if(phone1Field.getText().length() >= 11){ //checking phone number isnt too long or short
+        
+            errorMessage.setText("Phone number must be 7 - 11");
+            errorMessage.setVisible(true);
+            return;
+        }
+
         System.out.println("name: " + name);
         System.out.println("phone1: " + phone1Field.getText());
         System.out.println("phone2: " + phone2Field.getText());
         System.out.println("email: " + email);
         System.out.println("address: " + address);
         System.out.println("description: " + description);
-        
+
         Client newClient = new Client(name, description, phone1Field.getText(), phone2Field.getText(), email, address, true);
-        
+
         Main.jdbcc.persistClient(newClient); //persist to db
 
         Parent root = FXMLLoader.load(getClass().getResource("/ui/HomePageGUI.fxml")); //reroute user to home page
@@ -87,5 +104,4 @@ public class CreateClientGUIController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
-
 }
