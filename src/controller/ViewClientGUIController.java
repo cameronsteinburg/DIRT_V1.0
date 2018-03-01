14cb419ac776b3999ef3db2acf4c52dcf41272c6
645011 <645011@ELW7IT215937.ACDM.DS.SAIT.CA>
2018-03-01 13:03:10 -0700

    Kevin: Confirmation box created

--- src/controller/ViewClientGUIController.java
+++ src/controller/ViewClientGUIController.java
@@ -2,12 +2,16 @@ package controller;
 
 import entity.Client;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -40,55 +44,61 @@ public class ViewClientGUIController implements Initializable {
     private TableColumn<?, ?> secondNumCol;
     @FXML
     private TableColumn<?, ?> emailCol;
-    
-    
-    
+
     @FXML
-    private void editClientAction(ActionEvent event){
-        
-        
+    private void editClientAction(ActionEvent event) {
+
     }
-    
+
     /**
-     * 
-     * @param event 
+     *
+     * @param event
      */
     @FXML
-    private void removeClientAction(ActionEvent event){
-        
-        if (this.selectedClient != null){
-        
-            DBServices dbs = new DBServices();
-            
-            Client target = dbs.getClient(this.selectedClient.getName());
-            String name = target.getName();
-            dbs.deleteClient(target); 
-            this.updateTable();
-            HomePageGUIController.setBannerMessage("Client " + name + " Successfully Removed");
+    private void removeClientAction(ActionEvent event) {
+
+        if (this.selectedClient != null) {
+
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Delete Client Confirmation");
+            alert.setHeaderText("Confirm Deletion");
+            alert.setContentText("Delete client with the name: " + selectedClient.getName() + "?");
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK) {
+                DBServices dbs = new DBServices();
+
+                Client target = dbs.getClient(this.selectedClient.getName());
+                String name = target.getName();
+                dbs.deleteClient(target);
+                this.updateTable();
+                HomePageGUIController.setBannerMessage("Client " + name + " Successfully Removed");
+            } else {
+                alert.close();
+            }
         }
     }
-    
-    
+
     /**
-     * 
-     * @param event 
+     *
+     * @param event
      */
     @FXML
     private void getSelectedClient(MouseEvent event) {
-        
+
         if (clientTable.getSelectionModel().getSelectedItem() != null) {
-            
+
             this.selectedClient = clientTable.getSelectionModel().getSelectedItem();
-            
+
             //Enable buttons once client is selected
             viewClientBtn.setDisable(false);
             editClientBtn.setDisable(false);
             removeClientBtn.setDisable(false);
         }
     }
-    
-    private void updateTable(){
-        
+
+    private void updateTable() {
+
         nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
         addressCol.setCellValueFactory(new PropertyValueFactory<>("address"));
         firstNumCol.setCellValueFactory(new PropertyValueFactory<>("phone1"));
@@ -111,5 +121,4 @@ public class ViewClientGUIController implements Initializable {
         this.updateTable();
     }
 
-    
 }
