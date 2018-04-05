36c4aebc97281c695b328f045cbcab3441ffa3e1
734972 <734972@ELF6IT215940.ACDM.DS.SAIT.CA>
2018-04-05 14:20:00 -0600

    Cam: continuing work on project page 3

--- src/persistence/DBAccessor.java
+++ src/persistence/DBAccessor.java
@@ -1,17 +1,13 @@
 package persistence;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.filechooser.FileSystemView;
