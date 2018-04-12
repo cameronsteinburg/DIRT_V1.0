ec9862185d74f487b4319beec54f070a8bb5e32d
734972 <734972@ELF6IT215940.ACDM.DS.SAIT.CA>
2018-04-12 13:08:27 -0600

    Cam: WO_RetWall

---
+++ src/entity/Services/WO_RetWall.java
@@ -0,0 +1,235 @@
+package entity.Services;
+
+import entity.WorkOrder;
+
+/**
+ *
+ * @author 734972
+ */
+public class WO_RetWall extends WorkOrder {
+
+    private double estLineFT;
+    private double estHeight;
+    private double estBaseDepth;
+    private double estBaseWidth;
+    private double estSQFT;
+    private double estBaseReqYards;
+    private double estBaseSupply;
+    private double estBaseHours;
+    private double estBaseLabour;
+    private double estBaseRowHours;
+    private double estBaseRowLabour;
+    private double estBlock;
+    
+    private double actLineFT;
+    private double actHeight;
+    private double actBaseDepth;
+    private double actBaseWidth;
+    private double actSQFT;
+    private double actBaseReqYards;
+    private double actBaseSupply;
+    private double actBaseHours;
+    private double actBaseLabour;
+    private double actBaseRowHours;
+    private double actBaseRowLabour;
+    private double actBlock;
+
+    public WO_RetWall(boolean active) {
+        this.isActive = isActive;
+    }
+
+    public double getEstLineFT() {
+        return estLineFT;
+    }
+
+    public void setEstLineFT(double estLineFT) {
+        this.estLineFT = estLineFT;
+    }
+
+    public double getEstHeight() {
+        return estHeight;
+    }
+
+    public void setEstHeight(double estHeight) {
+        this.estHeight = estHeight;
+    }
+
+    public double getEstBaseDepth() {
+        return estBaseDepth;
+    }
+
+    public void setEstBaseDepth(double estBaseDepth) {
+        this.estBaseDepth = estBaseDepth;
+    }
+
+    public double getEstBaseWidth() {
+        return estBaseWidth;
+    }
+
+    public void setEstBaseWidth(double estBaseWidth) {
+        this.estBaseWidth = estBaseWidth;
+    }
+
+    public double getEstSQFT() {
+        return estSQFT;
+    }
+
+    public void setEstSQFT(double estSQFT) {
+        this.estSQFT = estSQFT;
+    }
+
+    public double getEstBaseReqYards() {
+        return estBaseReqYards;
+    }
+
+    public void setEstBaseReqYards(double estBaseReqYards) {
+        this.estBaseReqYards = estBaseReqYards;
+    }
+
+    public double getEstBaseSupply() {
+        return estBaseSupply;
+    }
+
+    public void setEstBaseSupply(double estBaseSupply) {
+        this.estBaseSupply = estBaseSupply;
+    }
+
+    public double getEstBaseHours() {
+        return estBaseHours;
+    }
+
+    public void setEstBaseHours(double estBaseHours) {
+        this.estBaseHours = estBaseHours;
+    }
+
+    public double getEstBaseLabour() {
+        return estBaseLabour;
+    }
+
+    public void setEstBaseLabour(double estBaseLabour) {
+        this.estBaseLabour = estBaseLabour;
+    }
+
+    public double getEstBaseRowHours() {
+        return estBaseRowHours;
+    }
+
+    public void setEstBaseRowHours(double estBaseRowHours) {
+        this.estBaseRowHours = estBaseRowHours;
+    }
+
+    public double getEstBaseRowLabour() {
+        return estBaseRowLabour;
+    }
+
+    public void setEstBaseRowLabour(double estBaseRowLabour) {
+        this.estBaseRowLabour = estBaseRowLabour;
+    }
+
+    public double getEstBlock() {
+        return estBlock;
+    }
+
+    public void setEstBlock(double estBlock) {
+        this.estBlock = estBlock;
+    }
+
+    public double getActLineFT() {
+        return actLineFT;
+    }
+
+    public void setActLineFT(double actLineFT) {
+        this.actLineFT = actLineFT;
+    }
+
+    public double getActHeight() {
+        return actHeight;
+    }
+
+    public void setActHeight(double actHeight) {
+        this.actHeight = actHeight;
+    }
+
+    public double getActBaseDepth() {
+        return actBaseDepth;
+    }
+
+    public void setActBaseDepth(double actBaseDepth) {
+        this.actBaseDepth = actBaseDepth;
+    }
+
+    public double getActBaseWidth() {
+        return actBaseWidth;
+    }
+
+    public void setActBaseWidth(double actBaseWidth) {
+        this.actBaseWidth = actBaseWidth;
+    }
+
+    public double getActSQFT() {
+        return actSQFT;
+    }
+
+    public void setActSQFT(double actSQFT) {
+        this.actSQFT = actSQFT;
+    }
+
+    public double getActBaseReqYards() {
+        return actBaseReqYards;
+    }
+
+    public void setActBaseReqYards(double actBaseReqYards) {
+        this.actBaseReqYards = actBaseReqYards;
+    }
+
+    public double getActBaseSupply() {
+        return actBaseSupply;
+    }
+
+    public void setActBaseSupply(double actBaseSupply) {
+        this.actBaseSupply = actBaseSupply;
+    }
+
+    public double getActBaseHours() {
+        return actBaseHours;
+    }
+
+    public void setActBaseHours(double actBaseHours) {
+        this.actBaseHours = actBaseHours;
+    }
+
+    public double getActBaseLabour() {
+        return actBaseLabour;
+    }
+
+    public void setActBaseLabour(double actBaseLabour) {
+        this.actBaseLabour = actBaseLabour;
+    }
+
+    public double getActBaseRowHours() {
+        return actBaseRowHours;
+    }
+
+    public void setActBaseRowHours(double actBaseRowHours) {
+        this.actBaseRowHours = actBaseRowHours;
+    }
+
+    public double getActBaseRowLabour() {
+        return actBaseRowLabour;
+    }
+
+    public void setActBaseRowLabour(double actBaseRowLabour) {
+        this.actBaseRowLabour = actBaseRowLabour;
+    }
+
+    public double getActBlock() {
+        return actBlock;
+    }
+
+    public void setActBlock(double actBlock) {
+        this.actBlock = actBlock;
+    }
+
+    
+    
+}
