6b36f6b66b24ee1a96c061d21e076c8db49de95a
734972 <734972@ELF6IT215940.ACDM.DS.SAIT.CA>
2018-04-12 12:36:48 -0600

    Cam: bed fix

--- src/controller/CreateProjectGUI_3Controller.java
+++ src/controller/CreateProjectGUI_3Controller.java
@@ -348,6 +348,18 @@ public class CreateProjectGUI_3Controller extends Controller implements Initiali
                         TextField serTotal = (TextField) bed.get(17);
                         Double serTotalDbl = aggMatCost + labourCostDouble;
                         serTotal.setText(f.format(serTotalDbl));
+                        
+                        newBed.setEstSQFT(sqftdbl);
+                        newBed.setEstHours(manHoursDbl);
+                        newBed.setEstLabour(labourCostDouble);
+                        newBed.setEstReqYards(reqyardsdbl);
+                        newBed.setQuotedTotal(serTotalDbl);
+                        
+                        newBed.setActSQFT(sqftdbl);
+                        newBed.setActHours(manHoursDbl);
+                        newBed.setActLabour(labourCostDouble);
+                        newBed.setActReqYards(reqyardsdbl);
+                        newBed.setActualTotal(serTotalDbl);
 
                         botCheck();
                     }
